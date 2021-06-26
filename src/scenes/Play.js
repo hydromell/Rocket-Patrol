8138d4a7c5b976b560b536ac7a0fb3b62807c543
hydromell <memeliu@ucsc.edu>
2021-06-25 22:04:30 -0700

    menu play scene

---
+++ src/scenes/Play.js
@@ -0,0 +1,19 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    init() {
+
+    }
+    preload() {
+
+    }
+    create() {
+        this.add.text(20, 20, "Rocket Patrol Play");
+
+    }
+    update() {
+
+    }
+}
