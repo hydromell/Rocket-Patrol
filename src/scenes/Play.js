de95c182a67ae094a4ed8788386e267c83aab75b
hydromell <memeliu@ucsc.edu>
2021-06-27 08:52:06 -0700

    added rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -26,6 +26,11 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
 
+        this.p1Rocket = new Rocket(this,
+            game.config.width / 2,
+            game.config.height - (borderUISize + borderPadding),
+            'rocket').setOrigin(0.5, 0);
+
     }
     update() {
         this.starfield.tilePositionX -= 4;
